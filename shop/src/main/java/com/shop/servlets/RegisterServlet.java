7b5b10c7e1a977f8fe469fdb9f4200ee7609f3d0
koka <koka@ELAD>
2015-03-08 09:25:07 +0200

    started with Spring MVC. MVC Started

--- shop/src/main/java/com/shop/servlets/RegisterServlet.java
+++ shop/src/main/java/com/shop/servlets/RegisterServlet.java
@@ -18,7 +18,7 @@ public class RegisterServlet extends HttpServlet {
 	public RegisterServlet() {
 		super();
 	}
-
+	
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("RegisterServlet doGet");
