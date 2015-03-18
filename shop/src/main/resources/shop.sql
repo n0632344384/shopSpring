85356438e53b9030c96a96ad8fb6ef1abc6ba0ce
koka <koka@ELAD>
2015-03-18 13:30:52 +0200

    First commit from spring mvc + security

--- shop/src/main/resources/shop.sql
+++ shop/src/main/resources/shop.sql
@@ -143,7 +143,7 @@ DROP TABLE IF EXISTS `roles`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roles` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `name` enum('USER','MANDATOR','ADMIN') default NULL,
+  `name` enum('ROLE_USER','ROLE_MANDATOR','ADMIN') default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
